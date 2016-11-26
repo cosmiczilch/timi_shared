2ec56e9f45e2d418b4e635d710458ce9ca9ce94f
cosmiczilch <cosmiczilch@gmail.com>
2016-11-25 22:30:14 -0800

    PrefabLoader mostly works.

--- Shared/Scripts/Init/SharedInit.cs
+++ Shared/Scripts/Init/SharedInit.cs
@@ -1,3 +1,4 @@
+using TimiShared.Debug;
 using TimiShared.Loading;
 using TimiShared.Service;
 using UnityEngine;
@@ -5,9 +6,15 @@ using UnityEngine;
 namespace TimiShared.Init {
     public class SharedInit : MonoBehaviour, IInitializable {
 
+        [SerializeField] private PrefabLoader _prefabloader;
+
         #region IInitializable
         public void StartInitialize() {
-            ServiceLocator.RegisterService<PrefabLoader>(new PrefabLoader());
+            if (this._prefabloader != null) {
+                ServiceLocator.RegisterService<PrefabLoader>(this._prefabloader);
+            } else {
+                TimiDebug.LogErrorColor("No prefab loader configured", LogColor.red);
+            }
         }
 
         public bool IsFullyInitialized() {
--- Shared/Scripts/Loading/PrefabLoader.cs
+++ Shared/Scripts/Loading/PrefabLoader.cs
@@ -1,7 +1,56 @@
+using System.Collections;
+using TimiShared.Debug;
 using TimiShared.Service;
+using UnityEngine;
 
 namespace TimiShared.Loading {
-    public class PrefabLoader : IService {
+    // TODO: Change this to not be a MonoBehaviour; right now the only reason this is a MonoBehaviour is for this.StartCoroutine
+    // TODO: Add templated loaders
+    public class PrefabLoader : MonoBehaviour, IService {
 
+        public static PrefabLoader Instance {
+            get {
+                return ServiceLocator.Service<PrefabLoader>();
+            }
+        }
+
+        #region Public API
+        public GameObject InstantiateSynchronous(string path, Transform parent) {
+            Object prefab = this.LoadPrefabSynchronous(path);
+            GameObject instance = Instantiate(prefab) as GameObject;
+            instance.transform.SetParent(parent, worldPositionStays: false);
+            return instance;
+        }
+
+        public void InstantiateAsynchronous(string path, Transform parent, System.Action<GameObject> callback) {
+            this.LoadPrefabAsync(path, (Object prefab) => {
+                GameObject instance = Instantiate(prefab) as GameObject;
+                instance.transform.SetParent(parent, worldPositionStays: false);
+                if (callback != null) {
+                    callback.Invoke(instance);
+                }
+            });
+        }
+
+        public Object LoadPrefabSynchronous(string path) {
+            Object prefab = Resources.Load(path);
+            return prefab;
+        }
+
+        public void LoadPrefabAsync(string path, System.Action<Object> callback) {
+            this.StartCoroutine(this.LoadAsyncInternal(path, callback));
+        }
+        #endregion
+
+        private IEnumerator LoadAsyncInternal(string path, System.Action<Object> callback) {
+            ResourceRequest request = Resources.LoadAsync(path);
+            yield return request;
+            if (request.asset == null) {
+                TimiDebug.LogErrorColor("Failed to load resource at path: " + path, LogColor.red);
+            }
+            if (callback != null) {
+                callback.Invoke(request.asset);
+            }
+        }
     }
 }
